561ce8037258c12917b6e0ca68b1d16d23b38c46
Joker Banny <jokerbanny@gmail.com>
2023-02-22 14:53:47 +0700

    success protect-authorize

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -12,18 +12,13 @@ const router = Router();
 
 router
   .route("/")
-  .get(getUsers)
-  // .get(protect, authorize("ADMIN"), getUsers)
-  .post(createUser);
-// .post(protect, authorize("ADMIN"), createUser);
+  .get(protect, authorize("ADMIN"), getUsers)
+  .post(protect, authorize("ADMIN"), createUser);
 
 router
   .route("/:id")
-  .get(getUser)
-  // .get(protect, authorize("ADMIN"), getUser)
-  .put(updateUser)
-  // .put(protect, authorize("ADMIN"), updateUser)
-  .delete(deleteUser);
-// .delete(protect, authorize("ADMIN"), deleteUser);
+  .get(protect, authorize("ADMIN"), getUser)
+  .put(protect, authorize("ADMIN"), updateUser)
+  .delete(protect, authorize("ADMIN"), deleteUser);
 
 export default router;
