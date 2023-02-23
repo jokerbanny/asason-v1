faa5c9733cddfee6245ae23c0bba2b99bddb89aa
Joker Banny <jokerbanny@gmail.com>
2023-02-23 21:31:22 +0700

    success order-order-etail

---
+++ src/controllers/orderController.ts
@@ -0,0 +1,129 @@
+import prisma from "../utils/prisma";
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+
+interface OrderDetails {
+  orderDetails: OrderDetail[];
+}
+
+interface OrderDetail {
+  courseId: string;
+  courseType: string;
+  total: number;
+}
+
+//@desc      Get all Orders
+//route      GET /api/v1/orders
+//@access    Private
+export const getOrders = asyncHandler(async (req: Request, res: Response) => {
+  const orders = await prisma.orders.findMany({
+    include: {
+      orderDetail: true,
+      enrolleds: true,
+    },
+  });
+  res.status(200).json(orders);
+});
+
+//@desc      Get all Orders
+//route      GET /api/v1/orders
+//@access    Private
+export const getOrder = asyncHandler(async (req: Request, res: Response) => {
+  const order = await prisma.orders.findUnique({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      orderDetail: true,
+      enrolleds: true,
+    },
+  });
+
+  if (!order) {
+    throw new Error(`not found order with id ${req.params.id}`);
+  }
+
+  res.status(200).json(order);
+});
+
+//@desc      Post Create Order
+//route      POST /api/v1/order
+//@access    Private
+export const createOrder = asyncHandler(async (req: Request, res: Response) => {
+  const { imagePayment, subTotal, total, courseItems } = req.body;
+  const order = await prisma.orders.create({
+    data: {
+      // @ts-ignore
+      userId: req.user.id,
+      imagePayment,
+      subTotal,
+      total,
+      orderDetail: {
+        create: courseItems.map((item: OrderDetails) => item),
+      },
+    },
+    include: {
+      orderDetail: true,
+    },
+  });
+
+  res.status(201).json(order);
+});
+
+//@desc      Put Update Order
+//route      PUT /api/v1/orders/id
+//@access    Private
+export const updateOrder = asyncHandler(async (req: Request, res: Response) => {
+  let order = await prisma.orders.findUnique({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      orderDetail: true,
+    },
+  });
+
+  if (!order) {
+    throw new Error(`not found order with id ${req.params.id}`);
+  }
+
+  // Update Order To Success Payment
+  if (req.body.status === "SUCCESS") {
+    // @ts-ignore
+    order = await prisma.orders.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        status: req.body.status,
+        enrolleds: {
+          // @ts-ignore
+          create: order.orderDetail?.map((item: OrderDetail) => {
+            return {
+              userId: order?.userId,
+              courseId: item.courseId,
+            };
+          }),
+        },
+      },
+      include: {
+        enrolleds: true,
+        orderDetail: true,
+      },
+    });
+  }
+  res.status(201).json(order);
+});
+
+//@desc      Delete Update Order
+//route      DELETE /api/v1/orders/id
+//@access    Private
+export const deleteOrder = asyncHandler(async (req: Request, res: Response) => {
+  await prisma.orders.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json("Deleted order success");
+});
